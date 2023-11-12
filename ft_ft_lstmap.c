cf9a3c1b3bf1447495f376620cb44ed9030af7ee
ERROR244 <khalilsohal24@gmail.com>
2023-11-12 13:26:32 +0100

    ...

---
+++ ft_ft_lstmap.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft_lstmap.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksohail- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/12 13:09:19 by ksohail-          #+#    #+#             */
+/*   Updated: 2023/11/12 13:25:55 by ksohail-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_list;
+	t_list	*curr;
+	t_list	*new_node;
+	void	*content_res;
+
+	new_list = NULL;
+	while (lst != NULL)
+	{
+		content_res = f(lst->content);
+		new_node = ft_lstnew(content_res);
+		if (new_node == NULL)
+			ft_lstclear(new_list, del);
+		ft_lstadd_back(&new_list, new_node);
+		lst = lst->next;
+	}
+	return (new_list);
+}
