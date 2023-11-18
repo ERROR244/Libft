4c98f4c4352bcc2768fadf9c84939a9d0864a6fb
ERROR244 <khalilsohail24@gmail.com>
2023-11-18 19:15:15 +0100

    ...

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ksohail- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ksohail- <ksohail-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 10:42:46 by ksohail-          #+#    #+#             */
-/*   Updated: 2023/11/06 19:26:03 by ksohail-         ###   ########.fr       */
+/*   Updated: 2023/11/18 10:14:59 by ksohail-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	{
 		if (*s1 != *s2)
 			return ((unsigned char)(*s1) - (unsigned char)(*s2));
-		if (*s1 == '\0' || *s2 == '\0')
+		if (*s1 == '\0' && *s2 == '\0')
 			break ;
 		s1++;
 		s2++;
